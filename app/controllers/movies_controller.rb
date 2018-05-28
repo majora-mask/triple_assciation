cb1e7ec3c9b99886db8e2a5b779418884749f836
phantomspecial <win.2000@live.jp>
2018-05-29 00:51:09 +0900

    Refactoring

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,8 +14,7 @@ class MoviesController < ApplicationController
   private
 
   def hash_maker(attr_typ, set_val, st_ac, st_ma, st_mo)
-    { no: '',
-      movie_table_id: st_mo.id, movie_table_name: st_mo.name,
+    { movie_table_id: st_mo.id, movie_table_name: st_mo.name,
       movie_actor_table_id:  st_ma.id, movie_actor_table_movie_id: st_ma.movie_id, movie_actor_table_actor_id: st_ma.actor_id,
       actor_table_id: st_ac.id, actor_table_name: st_ac.name, actor_table_age: st_ac.age, actor_table_weight: st_ac.weight, actor_table_height: st_ac.height,
       setting_table_id: set_val.id, setting_table_setting_value: set_val.setting_value, setting_table_attribute_type_id: set_val.attribute_type_id, setting_table_actor_id: set_val.actor_id,
